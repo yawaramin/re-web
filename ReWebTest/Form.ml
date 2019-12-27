929e59de35ad87f2ef929fc5b81be57b6c5174ce
Yawar Amin <yawar.amin@gmail.com>
2019-12-27 14:52:46 -0500

    Add form decoding tests

---
+++ ReWebTest/Form.ml
@@ -0,0 +1,24 @@
+open ReWeb.Form
+
+type user = { id : int; name : string }
+
+let user id name = { id; name }
+
+let form = make
+  Field.[int "id"; "name" |> string |> ensure ((<>) "")]
+  user
+
+let%test "decoder - decode correctly" =
+  match decoder form "id=1&name=Bob" with
+  | Ok { id = 1; name = "Bob" } -> true
+  | _ -> false
+
+let%test "decoder - error on bad input" = match decoder form "" with
+  | Error "ReWeb.Form.decoder: could not find key id" -> true
+  | _ -> false
+
+let%test "ensure - error on validation fail" =
+  match decoder form "id=1&name=" with
+  | Error "ReWeb.Form.Field.ensure: name" -> true
+  | _ -> false
+
