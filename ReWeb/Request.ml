yawaramin/re-web

commit 5ea88fae0e38c608c598c97ae24d4b3f92c03382

Don't perform a bigarray copy of every body chunk (#36)

ReWeb/Request.ml
@@ -88,7 +88,7 @@ module Make
       push_to_stream (Some {
         H.IOVec.off;
         len;
-        buffer = Bigstringaf.copy buffer ~off ~len
+        buffer
       });
       B.schedule_read request_body ~on_eof ~on_read
     in

commit 9f5ab9ca0e36b20dcdc0fa12644d0e31626ca7ef

Fix #38
Seems to have been caused by #36. Httpaf seems to still reuse the same
buffer for every chunk, so copy the buffer to get the request body
safely.

However, use @anmonteiro 's suggestion to set the offset to 0.

ReWeb/Request.ml
@@ -99,7 +99,7 @@ module Make
       push_to_stream (Some {
         H.IOVec.off;
         len;
-        buffer
+        buffer = Bigstringaf.copy buffer ~off:0 ~len;
       });
       B.schedule_read request_body ~on_eof ~on_read
     in